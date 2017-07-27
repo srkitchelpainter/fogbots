639b5e45b9dcfb8f31179709d138b360d5c33027
Shannon Kitchel-Painter <30448647+srkitchelpainter@users.noreply.github.com>
2017-07-27 15:13:09 -0600

    Update Login.aspx.cs

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -1,85 +1,61 @@
-<%@ Page Title="Log in" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Account_Login" Async="true" %>
+using System;
+using System.Data.SqlClient;
 
-<%@ Register Src="~/Account/OpenAuthProviders.ascx" TagPrefix="uc" TagName="OpenAuthProviders" %>
+/// <summary>
+/// Log in functions for user login 
+/// </summary>
+/// 
 
-<%-- 
 
-TITLE: Login.aspx
+public partial class Account_Login : System.Web.UI.Page
+{
+  protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["UserName"] != null)
+        {
+            Response.Redirect("Account.aspx");
+        }
+    }
 
-AUTHOR: Shannon Painter
+    protected void LogIn(object sender, EventArgs e)
+    {
+        SqlConnection vid = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Fogbots;Integrated Security=True");
+        vid.Open();
+        string checkuser = "Select count(*) from Users where email='" + UserName.Text + "'";
+        SqlCommand com = new SqlCommand(checkuser, vid);
+        int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+        vid.Close();
+        if (temp == 1)
+        {
+            vid.Open();
+            string checkpassword = "Select password from Users where email='" + UserName.Text + "'";
+            SqlCommand passcom = new SqlCommand(checkpassword, vid);
+            string password = passcom.ExecuteScalar().ToString();
+            if (password == Password.Text)
+            {
+                {
+                    SqlCommand isapproved = new SqlCommand("Update Users set isApproved = '" + "1" + "' where email = '" + UserName.Text + "'", vid);
+                    isapproved.Parameters.AddWithValue("@isApproved", "1");
+                    isapproved.ExecuteNonQuery();
+                    Session["UserName"] = UserName.Text;
+                    Response.Redirect("Account.aspx");
+                    vid.Close();
+                }
 
-PURPOSE: Fogbots Log in Page
 
-ORIGINALLY CREATED ON: 27 July 2017
+            }
 
-LAST MODIFIED ON: 
+            else
+            {
+                loginError.Text = ("Password is incorrect");
+            }
+        }
+        else
+        {
+            loginError.Text = ("User name is incorrect.");
+        }
+    }
+}
 
-LAST MODIFIED BY: 
 
-MODIFICATION HISTORY: 
-
-    --%>
-
-<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
-    
-    <div id="allcontent">
-    
-    <h2><%: Title %>.</h2>
-
-    <div class="row">
-        <div class="col-md-8">
-            <section id="loginForm">
-                <div class="form-horizontal">
-                    <h4></h4>
-                    <hr />
-                    <asp:PlaceHolder runat="server" ID="ErrorMessage" Visible="false">
-                        <p class="text-danger">
-                            <asp:Literal runat="server" ID="FailureText" />
-                        </p>
-                    </asp:PlaceHolder>
-                    <div class="form-group">
-                        <asp:Label runat="server" AssociatedControlID="UserName" CssClass="col-md-2 control-label">Email Address</asp:Label>
-                        <div class="col-md-10">
-                            <asp:TextBox runat="server" ID="UserName" CssClass="form-control" />
-                            <asp:RequiredFieldValidator runat="server" ControlToValidate="UserName"
-                                CssClass="text-danger" ErrorMessage="Email address is required." />
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <asp:Label runat="server" AssociatedControlID="Password" CssClass="col-md-2 control-label">Password</asp:Label>
-                        <div class="col-md-10">
-                            <asp:TextBox runat="server" ID="Password" TextMode="Password" CssClass="form-control" />
-                            <asp:RequiredFieldValidator runat="server" ControlToValidate="Password" CssClass="text-danger" ErrorMessage="The password field is required." />
-                        </div>
-                    </div>
-                    <%--<div class="form-group">
-                        <div class="col-md-offset-2 col-md-10">
-                            <div class="checkbox">
-                                <asp:CheckBox runat="server" ID="RememberMe" />
-                                <asp:Label runat="server" AssociatedControlID="RememberMe">Remember me?</asp:Label>
-                            </div>
-                        </div>
-                    </div>--%>
-                    <div class="form-group">
-                        <div class="col-md-offset-2 col-md-10">
-                            <asp:Button runat="server" OnClick="LogIn" Text="Log in" CssClass="btn btn-default" />
-                        </div>
-                    </div>
-                </div>
-                <p>
-                    <asp:HyperLink runat="server" ID="RegisterHyperLink" ViewStateMode="Disabled" NavigateUrl="~/Account/Register.aspx">Register here</asp:HyperLink>
-                    if you don't have an account.
-                </p>
-            </section>
-        </div>
-
-      <%--  <div class="col-md-4">
-            <section id="socialLoginForm">
-                <uc:openauthproviders runat="server" id="OpenAuthLogin" />
-            </section>
-        </div>--%>
-    </div>
-
-        </div>
-</asp:Content>
 
