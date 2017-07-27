6caa3b90511967eac3c136941e2e4d6324417d78
Shannon Kitchel-Painter <30448647+srkitchelpainter@users.noreply.github.com>
2017-07-27 14:56:14 -0600

    Update Register.aspx.cs

--- Register.aspx.cs
+++ Register.aspx.cs
@@ -20,39 +20,26 @@ public partial class Account_Register : System.Web.UI.Page
 
     protected void CreateUser_Click(object sender, EventArgs e)  //sends registration data to SQL Server
     {
-       
-      SqlConnection vid = new SqlConnection("Data Source=IN-CSCI-ASPNET;Initial Catalog=shkitche;Persist Security Info=True;User ID=shkitche;Password=s8414kit");
-        {
-                SqlCommand xp = new SqlCommand("Insert into Customers(FirstName, MiddleName, LastName, StreetAddress, City, State, ZIP, Email, Password, IsApproved) Values(@FirstName, @MiddleName, @LastName, @StreetAddress, @City, @State, @ZIP, @Email, @Password, @IsApproved)", vid);
-                
-                xp.Parameters.AddWithValue("@FirstName", fname.Text);
-                xp.Parameters.AddWithValue("@MiddleName", mname.Text);
-                xp.Parameters.AddWithValue("@LastName", lname.Text);
-                xp.Parameters.AddWithValue("@StreetAddress", address.Text);
-                xp.Parameters.AddWithValue("@City", address.Text);
-                xp.Parameters.AddWithValue("@State", DropDownList1.SelectedValue);
-                xp.Parameters.AddWithValue("@ZIP", ZIP.Text);
-                xp.Parameters.AddWithValue("@Email", UserName.Text);
-                xp.Parameters.AddWithValue("@Password", Password.Text);
-                xp.Parameters.AddWithValue("@IsApproved", "0");
 
-                vid.Open();
-                xp.ExecuteNonQuery();
-                vid.Close();
-        
-                }
-                       
+        SqlConnection vid = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=" ";Integrated Security=True");
+        {
+            SqlCommand xp = new SqlCommand("Insert into Users(email, password, isApproved) Values(@email, @password, @isApproved)", vid);
+
+            xp.Parameters.AddWithValue("@email", email.Text);
+            xp.Parameters.AddWithValue("@password", password.Text);
+            xp.Parameters.AddWithValue("@isApproved", "1");
+
+            vid.Open();
+            xp.ExecuteNonQuery();
+            vid.Close();
+
+        }
+
 
         if (IsPostBack)
         {
-            fname.Text = "";
-            mname.Text = "";
-            lname.Text = "";
-            address.Text = "";
-            city.Text = "";
-            ZIP.Text = "";
-            UserName.Text = "";
-            Password.Text = "";
+            email.Text = "";
+            password.Text = "";
         }
     }
 }
